dab8cff170aa17311281968b332afd3fc1961ea4
Chanchal Soni <chanchal@Chanchals-MacBook-Pro.local>
2021-08-08 02:15:24 +0530

    add tails implementation

--- src/app.js
+++ src/app.js
@@ -2,4 +2,9 @@ function heads(arr) {
     return arr[0];
 }
 
-module.exports = {heads}
+function tails(arr) {
+    return arr.length === 0 ? undefined : arr.slice(1)
+}
+
+
+module.exports = {heads,tails}
--- src/app.test.js
+++ src/app.test.js
@@ -8,4 +8,13 @@ test('returns head of {} as undefined', () => {
     expect(app.heads([])).toBe(undefined);
 });
 
+test('returns tail of {1,2,3} as 2,3', () => {
+    expect(app.tails([1, 2, 3])).toStrictEqual([2, 3]);
+});
+
+test('returns tail of {} as undefined', () => {
+    expect(app.tails([])).toBe(undefined);
+});
+
+
 
